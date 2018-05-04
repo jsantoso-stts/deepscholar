8b8918610f889a434f9ab17f3e172d90ebf4dea5
ya90net <ya90net@gmail.com>
2018-05-04 14:12:14 +0800

    Add importEntities / deleteEntities commands

---
+++ server/scripts/importEntities.js
@@ -0,0 +1,9 @@
+const common = require("./common.js");
+const DatabaseTools = require("../common/database_tools");
+
+(async () => {
+  	const config = await common.loadDeepScholarConfig();  	
+
+  	DatabaseTools.importEntities(config, process.stdin);
+  	
+})();
